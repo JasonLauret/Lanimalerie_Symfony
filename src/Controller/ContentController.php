237ae06e9900e4cc26f01a44e287e98a897c1aff
JasonLauret <80036076+JasonLauret@users.noreply.github.com>
2021-07-29 03:12:18 +0100

    Modification footer

--- src/Controller/ContentController.php
+++ src/Controller/ContentController.php
@@ -3,6 +3,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
@@ -12,15 +13,14 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ContentController extends AbstractController
 {
-    
     public function cgv(string $title): Response {
         return $this->render('content/cgv.html.twig', [
             'title' => $title,
             'description' => 'Contenu de la page',
         ]);
     }
-    
-    
+
+
     public function mentionLegal(string $title): Response {
         return $this->render('content/mentionLegal.html.twig', [
             'title' => $title,
@@ -28,7 +28,7 @@ class ContentController extends AbstractController
         ]);
     }
 
-    
+
     public function politique(string $title): Response {
         return $this->render('content/politique.html.twig', [
             'title' => $title,
@@ -36,26 +36,18 @@ class ContentController extends AbstractController
         ]);
     }
 
-    
-    public function home(): Response {
-        return $this->render('content/home.html.twig');
-    }
 
-    
     public function registration(): Response {
         return $this->render('content/registration.html.twig');
     }
 
-    
+
     public function pageLogin(): Response {
         return $this->render('content/pageLogin.html.twig');
     }
 
-    /**
-     * @IsGranted("ROLE_USER")
-     */
+
     public function setting(): Response {
         return $this->render('content/setting.html.twig');
     }
-
 }
---
+++ src/Controller/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CategoryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function home(CategoryRepository $categoryRepository): Response {
+        return $this->render('home/home.html.twig');
+    }
+}
