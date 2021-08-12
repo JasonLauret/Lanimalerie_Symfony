e0117ce54e24c47a3b1293f44fafc9b9e2121a87
JasonLauret <80036076+JasonLauret@users.noreply.github.com>
2021-08-12 14:53:02 +0100

    Ajout du formulaire de contact sur la page d'accueil et suppression du ContactController et du template contact.html.twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,37 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function home(CategoryRepository $categoryRepository): Response {
-        return $this->render('home/home.html.twig');
+    public function home(Request $request, MailerInterface $mailer): Response {
+
+        $form = $this->createForm(ContactType::class);
+
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()){
+            $data = $form->getData();
+            $message = (new Email())
+                ->from($data['email'])
+                ->to("lauret.jason73390@gmail.com")
+                ->subject('Demande en provenance du site')
+                ->text('From '. $data['email'].' '.$data['message'], 'text/plain');
+
+            $mailer->send($message);
+        }
+
+        return $this->render('home/home.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
