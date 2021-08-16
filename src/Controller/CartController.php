87ff893ae55c4ec41464da093291ff093ffbb39f
JasonLauret <80036076+JasonLauret@users.noreply.github.com>
2021-08-16 12:00:58 +0100

    Mise en place d'un panier

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
@@ -10,12 +11,31 @@ use Symfony\Component\Routing\Annotation\Route;
 class CartController extends AbstractController
 {
     #[Route('/cart', name: 'cart')]
-    public function index(): Response
+    public function index(SessionInterface $session, ProductRepository $productRepository): Response
     {
+        $panier = $session->get('panier', []);
+
+        $panierWithData = [];
+
+        foreach($panier as $id => $quantity) {
+            $panierWithData[] = [
+                'product' => $productRepository->find($id),
+                'quantity' => $quantity
+            ];
+        }
+        
+        $total = 0;
+        foreach($panierWithData as $item){
+            $totalItem = $item['product']->getPrice() * $item['quantity'];
+            $total += $totalItem;
+        }
+
         return $this->render('cart/index.html.twig', [
-            'controller_name' => 'CartController',
+            'items' => $panierWithData,
+            'total' => $total
         ]);
     }
+    
 
     #[Route('/cart/{id}', name: 'add_cart')]
 
@@ -31,6 +51,21 @@ class CartController extends AbstractController
 
         $session->set('panier', $panier);
 
-        dd($session->get('panier'));
+        return $this->redirectToRoute("cart");
+    }
+
+
+    #[Route('/cart/remove/{id}', name: 'remove_cart')]
+    public function removeCart($id, SessionInterface $session){
+        $panier = $session->get('panier', []);
+        if(!empty($panier[$id])) {
+            $panier[$id] -= 1;
+            if(!empty($panier[$id]) < 1) {
+                unset($panier[$id]);
+            }
+        }
+        $session->set('panier', $panier);
+
+        return $this->redirectToRoute("cart");
     }
 }
