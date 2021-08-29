f1fda191113e2e771f650c2ece4dce3e1d17fa4b
JasonLauret <80036076+JasonLauret@users.noreply.github.com>
2021-08-29 12:25:29 +0100

    Création page 'purchaseOrder' à finir

--- src/Controller/DeliveryController.php
+++ src/Controller/DeliveryController.php
@@ -2,79 +2,22 @@
 
 namespace App\Controller;
 
-use App\Entity\Order;
-use App\Entity\OrderProduct;
-use App\Repository\ProductRepository;
 use App\Service\Cart\CartService;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\RequestStack;
 
 class DeliveryController extends AbstractController
 {
     #[Route('/delivery', name: 'delivery')]
-    public function index(CartService $cartService,ProductRepository $productRepository, EntityManagerInterface $em)
+    public function index(CartService $cartService)
     {   
         $nbItem = $cartService->getFullCart();
         $nbItem = count($nbItem);
-
-        //creation new objet commande
-        $order = new Order();
-        //date du jour de la commande
-        $order->setDate(new \Datetime());
-        //qui a fait la commande (celui qui est connecter)
-        $order->setUser($this->getUser());
-        //recupere le panier dans la session
-        $session = $this->requestStack->getSession();
-        $panier = $session->get('panier');
-        //enregistre la commande
-        $em->persist($order);
-        
-        dump($panier);
-        //parcour le panier
-        foreach($panier as $key => $value){
-            //creation d'un nouveau objet orderProduct (nouvelle ligne )
-            $orderProduct = new OrderProduct();
-            //lie cette ligne a la commande
-            $order->addOrderProduct($orderProduct);
-            // recupere le produit a partir de l'id
-            $produit = $productRepository->find($key);
-            //lie le produit a la commande
-            $orderProduct->setProduct($produit);
-            //on lui donne la quantité
-            $orderProduct->setQuantity($value);
-            //enregistrement
-            $em->persist($orderProduct);
-            
-            dump($produit);
-        }
-        //execute les requetes
-        $em->flush();
 
         return $this->render('content/delivery.html.twig', [
             'items' => $cartService->getFullCart(),
             'total' => $cartService->getTotal(),
             'nbItem' => $nbItem
         ]);
-    }
-
-
-    #[Route('/delivery/test/{order}', name: 'app_admin_export_communication')]
-    public function exportCommunicationAction(Order $order)
-    {
-        dump($order);
-        foreach($order->getOrderProducts() as $elem){
-            dump($elem->getProduct()->getName());
-        }
-        die();
-    }
-
-
-    private $requestStack;
-
-    public function __construct(RequestStack $requestStack)
-    {
-        $this->requestStack = $requestStack;
     }
 }
---
+++ src/Controller/NewOrderController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Order;
+use App\Entity\OrderProduct;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class NewOrderController extends AbstractController
+{
+    #[Route('/new/order', name: 'new_order')]
+    public function index(ProductRepository $productRepository, EntityManagerInterface $em): Response
+    {
+        //_____Création de la commande_____
+
+        //Création new objet commande
+        $order = new Order();
+        //Date du jour de la commande
+        $order->setDate(new \Datetime());
+        //Qui à fait la commande (celui qui est connecter)
+        $order->setUser($this->getUser());
+        //Récupere le panier dans la session
+        $session = $this->requestStack->getSession();
+        $panier = $session->get('panier');
+        //Enregistre la commande
+        $em->persist($order);
+        
+        dump($panier);
+        //parcour le panier
+        foreach($panier as $key => $value){
+            //Création d'un nouveau objet orderProduct (nouvelle ligne dans orderProduct)
+            $orderProduct = new OrderProduct();
+            //Lie cette ligne à la commande
+            $order->addOrderProduct($orderProduct);
+            //Récupère le produit a partir de l'id
+            $produit = $productRepository->find($key);
+            //Lie le produit à la commande
+            $orderProduct->setProduct($produit);
+            //On lui donne la quantité
+            $orderProduct->setQuantity($value);
+            //Enregistrement
+            $em->persist($orderProduct);
+            
+            dump($produit);
+        }
+        //Exécution des requètes
+        $em->flush();
+
+
+        return $this->render('new_order/newOrder.html.twig', [
+            'controller_name' => 'NewOrderController',
+        ]);
+    }
+
+    #[Route('/delivery/test/{order}', name: 'app_admin_export_communication')]
+    public function exportCommunicationAction(Order $order)
+    {
+        dump($order);
+        foreach($order->getOrderProducts() as $elem){
+            echo $elem->getProduct()->getName();
+        }
+        die();
+    }
+
+
+    private $requestStack;
+
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+}
