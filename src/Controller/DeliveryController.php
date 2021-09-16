2ae553b8a03cb42021b81b4928770d568eb3967f
JasonLauret <80036076+JasonLauret@users.noreply.github.com>
2021-09-16 12:01:14 +0100

    Modification affichage delivery

--- src/Controller/DeliveryController.php
+++ src/Controller/DeliveryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\AdressRepository;
 use App\Service\Cart\CartService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +10,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class DeliveryController extends AbstractController
 {
     #[Route('/delivery', name: 'delivery')]
-    public function index(CartService $cartService)
+    public function index(CartService $cartService, AdressRepository $adressRepository)
     {   
         $nbItem = $cartService->getFullCart();
         $nbItem = count($nbItem);
+        
 
         return $this->render('order_tunnel/delivery.html.twig', [
+            'adresses' => $adressRepository->displayAdressById($this->getUser()),
             'items' => $cartService->getFullCart(),
             'total' => $cartService->getTotal(),
             'nbItem' => $nbItem
